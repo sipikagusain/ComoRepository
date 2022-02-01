37ed8821b853d56d406b8bfb39d065881e9b9f58
sipika gusain <sipika.gusain7@gmail.com>
2022-02-01 20:38:12 +0530

    changes

--- ReactApp/src/socialBladeGrid/userList.jsx
+++ ReactApp/src/socialBladeGrid/userList.jsx
@@ -160,7 +160,7 @@ class UserList extends React.Component {
     render() {
         return <>
             <div>
-                <h1>Welcome to Social Blade extracted data</h1>
+                <h1>Welcome to Social Blade extracted data !</h1>
                 <Grid>
                     <div style={{ marginTop: "30px" }}>
                         <Grid container item md={12}>
